34b3d3c96c6582a61b606826147e82b1466a14aa
Hayden Dippel <hdippel@wisc.edu>
2024-01-28 13:40:00 -0600

    slight wording changes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -28,12 +28,12 @@ function Projects() {
                     </ol>
                     <p>This all made viewing the court schedules very annoying, especially from your phone where you could not just CTRL+F basketball. The schedules frequently would change and, as an employee of one of the gyms, I would regularly have to tell members that they were out of luck as the sport they were hoping to play -- basketball, badminton, pickelball, futsal, or volleyball -- was not hosted by our courts that day.</p>
                 </div>
-                <img src={BakkeScheduleLedger} className='desktop-screenshot'/>
+                <img src={BakkeScheduleLedger} className='desktop-screenshot' style={{marginTop: '100px'}}/>
             </div>
             <div className='hbox mega-margin'>
                 <div className='invisible-card left'>
                     <h2 className='large-text medium-margin'>The Idea</h2>
-                    <p>I wanted to create a website that was <b>mobile friendly</b> and <b>easy to read</b>. The idea I ultimately landed on was to create a visual schedule, similar to google calendar with the courts at each gym.</p>
+                    <p>I wanted to create a website that was <b>mobile friendly</b> and <b>easy to read</b>. The idea I ultimately landed on was to create a visual schedule, similar to google calendar with the courts at each gym. The different sports would be color coded so that they would be easily recognized.</p>
                 </div>
                 <img src={WebsiteConcept} className='desktop-screenshot'/>
                 <img src={MobileAppConcept} className='desktop-screenshot'/>
@@ -42,10 +42,10 @@ function Projects() {
                 <div className='invisible-card left'>
                     <h2 className='large-text small-margin'>How I Built</h2>
                     <a href='www.uwopenrecroster.com'><h2 className='small-text large-margin'>UW Open Rec Roster</h2></a>
-                    <p className='large-margin'>The website is a vite react app hosted on DigitalOceans.com with a domain name from DreamHost.com. The website interacts with a backend to get the dialy schedules from the recwell API. These schedules are memoized serverside daily, as to not overwhelm the recwell API. Additionally, the backend logs requests to track our backend calls. </p>
+                    <p className='large-margin'>The website is a vite react app hosted on DigitalOceans.com with a domain name from DreamHost.com. The website interacts with a backend to get the dialy schedules from the recwell API. These schedules are memoized serverside daily, as to not overwhelm the recwell API. Additionally, the backend logs requests to track our backend calls. The DB was managed by my friend Bailey Kau.</p>
                     <h2 className='medium-text small-margin'>What did I learn while making the website?</h2>
                     <ul>
-                        <li><b>CSS Psuedo-Elements</b>: I was having serious issues with my syling and lag. Resizing the window would destroy the positioning of the hour mark lines and times themselves. I did some searching and found psuedo-elements `::before` and `::after`.</li>
+                        <li><b>CSS Psuedo-Elements</b>: I was having serious issues with my styling and lag. Resizing the window would destroy the positioning of the hour mark lines and times themselves. I did some searching and found psuedo-elements `::before` and `::after`.</li>
                         <li><b>CSS Animations</b>: I wanted to add a skeleton loading animation to my website to show that the data was loading. I was able to learn about animation past the transition attribute.</li>
                         <li><b>Deploying a Web-App and Backend</b>: This project was really the first time I needed to deploy a project. I learned how to deploy not just on the macro-scale with pre-built deployers, but using NGINX. For the backend I had a blank vm, and had to configure it. I also learned how to use domain names in this project, giving both my frontend and backend unique domain names.</li>
                     </ul>
