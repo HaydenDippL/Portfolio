527f0a07f7690d535a8822876279a6479e574101
Hayden Dippel <hdippel@wisc.edu>
2024-02-02 20:55:49 -0600

    sharpened hello world animation

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -3,43 +3,46 @@ import { useState, useEffect } from 'react'
 import '../styles.css'
 import '../styles/HelloWorld.css'
 
-const message = "Hello World!My name is Hayden..."
-const split = 12
-
-const timing = [
-    2500, // H
-    150, // e
-    200, // l
-    150, // l
-    200, // o
-    150, //  
-    250, // W
-    150, // o
-    150, // r
-    150, // l
-    150, // d
-    350, // !
-    1000, // M
-    150, // y
-    150, //  
-    150, // n
-    150, // a
-    150, // m
-    150, // e
-    150, //  
-    150, // i
-    150, // s
-    150, //  
-    150, // H
-    150, // a
-    150, // y
-    150, // d
-    150, // e
-    150, // n
-    150, // .
-    150, // .
-    150  // .
+const message = [
+    [2000, 'H'],//268
+    [192, 'e'],
+    [79, 'l'],
+    [131, 'l'],
+    [174, 'o'],
+    [157, ' '],
+    [213, 'W'],
+    [132, 'o'],
+    [102, 'r'],
+    [89, 'l'],
+    [112, 'd'],
+    [812, '!'],
+    [1200, 'Enter'],
+    [391, 'M'],
+    [257, 'y'],
+    [83, ' '],
+    [146, 'n'],
+    [508, 'a'],
+    [75, 'm'],
+    [95, 'e'],
+    [89, ' '],
+    [134, 'i'],
+    [88, 's'],
+    [77, ' '],
+    [239, 'H'],
+    [157, 'a'],
+    [67, 'y'],
+    [135, 'd'],
+    [66, 'e'],
+    [86, 'n'],
+    [239, '.'],
+    [218, '.'],
+    [219, '.'],
 ]
+
+let split
+message.forEach(([time, ch], i) => {
+    if (ch == 'Enter') split = i
+})
 
 function HelloWorld() {
     const [index, setIndex] = useState(0)
@@ -47,31 +50,29 @@ function HelloWorld() {
     useEffect(() => {
         if (index >= message.length) return
 
-        const timeoutID = setTimeout(() => setIndex(index + 1), timing[index])
+        const timeoutID = setTimeout(() => setIndex(index + 1), message[index][0])
         return () => clearTimeout(timeoutID)
     }, [index])
 
-    return <div className='full-screen light centered'>
-            {index < split ?
-                <>
-                    <div className='hbox'>
-                        <h1 className='title whitespace'>{message.substring(0, index)}</h1>
-                        <div className={`typer-line-title ${(timing[index] ?? Infinity) > 500 ? 'animated' : ''}`}></div>
-                    </div>
-                    <div className='hbox'>
-                        <h2 className='large-text'></h2>
-                    </div>
-                </> : 
-                <>
-                    <div className='hbox'>
-                        <h1 className='title whitespace'>{message.substring(0, split)}</h1>
-                    </div>
-                    <div className='hbox'>
-                        <h2 className='large-text whitespace'>{message.substring(split, index)}</h2>
-                        <div className={`typer-line-large ${(timing[index] ?? Infinity) > 500 ? 'animated' : ''}`}></div>
-                    </div>
-                </>
-            }
+    function get_message(start, end) {
+        return message.slice(start, end).reduce((acc, [time, ch]) => {
+            return acc + ch
+        }, '')
+    }
+
+    let animated = index >= message.length || message[index][0] > 500 ? 'animated' : ''
+    let blinker_upper = index <= split ? 'blinker' : ''
+    let blinker_lower = index > split ? 'blinker' : ''
+
+    return <div className='full-screen light center-horiz' style={{marginTop: '30vh'}}>
+        <div className='hbox'>
+            <h1 className='title whitespace'>{get_message(0, Math.min(index, split))}</h1>
+            {blinker_upper && <div className={`blinker blinker-title ${animated}`}/>}
+        </div>
+        <div className='hbox'>
+            <h2 className='large-text whitespace'>{get_message(split + 1, Math.min(index, message.length))}</h2> 
+            {blinker_lower && <div className={`blinker blinker-large ${animated}`}/>} 
+        </div>
     </div>
 }
 
