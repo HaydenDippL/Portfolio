0266a41f2dd6a3416046582289b912f1cf5c3816
Hayden Dippel <hdippel@wisc.edu>
2024-02-04 15:16:51 -0600

    styled badgerbasketball section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,10 +2,12 @@ import BakkeScheduleLedger from '../assets/BakkeScheduleLedger.png'
 import WebsiteConcept from '../assets/WebsiteConcept.jpg'
 import MobileAppConcept from '../assets/MobileAppConcept.jpg'
 import WebScreenShot from '../assets/WebScreenshot.png'
+import MobileScreenShot from '../assets/MobileScreenshot.png'
 
 import GitHubLink from './GitHubLink'
 
 import '../styles.css'
+import '../styles/Projects.css'
 
 function Projects() {
     return <>
@@ -13,44 +15,93 @@ function Projects() {
             <h1 className='title'>Projects</h1>
             <p className='large-text'>A quick overview of my work</p>
         </div>
-        <div className='dark left'>
+        <div className='dark centered'>
             <h1 className='title'>UW Open Rec Roster</h1>
             <GitHubLink link='https://github.com/HaydenDippL/BadgerBasketball'/>
-            <div className='hbox mega-margin'>
-                <div className='invisible-card left'>
-                    <h2 className='large-text medium-margin'>The Problem</h2>
-                    <p>My friends and I are avid basketball players and we found that the website which displayed the basketball court schedules was deficient.</p>
-                    <ol>
-                        <li>It took 4 links to navigate to it</li>
-                        <li>It usually didn't load properly on our phones</li>
-                        <li>It was ledger, making it hard to pinpoint just the basketball court availability</li>
-                        <li>You need to view two separate pages for the two different gyms</li>
+            <div className='hbox' style={{
+                marginTop: '30vh'   
+            }}>
+                <div className='left' style={{
+                    width: '40vw'
+                }}>
+                    <h2 className='small-text' style={{fontWeight: 200}}>THE PROBLEM</h2>
+                    <p className='large-text' style={{fontWeight: 700}}>The gym websites were extremely user-unfriendly</p>
+                    <ol className='small-text' style={{
+                        fontWeight: 500
+                    }}>
+                        <li>Hard to navigate</li>
+                        <li>Mobile Incompatibility</li>
+                        <li>Impossible to read</li>
+                        <li>Separate sites for both gyms</li>
                     </ol>
-                    <p>This all made viewing the court schedules very annoying, especially from your phone where you could not just CTRL+F basketball. The schedules frequently would change and, as an employee of one of the gyms, I would regularly have to tell members that they were out of luck as the sport they were hoping to play -- basketball, badminton, pickelball, futsal, or volleyball -- was not hosted by our courts that day.</p>
+                    <p></p>
                 </div>
-                <img src={BakkeScheduleLedger} className='desktop-screenshot' style={{marginTop: '100px'}}/>
+                <img src={BakkeScheduleLedger} style={{
+                    margin: '30px',
+                    width: '40vw',
+                    height: 'auto',
+                    borderRadius: '20px'
+                }}/>
+            </div>
+            <div className='hbox' style={{
+                marginTop: '25vh',
+                marginBottom: '25vh'
+            }}>
+                <img src={WebsiteConcept} style={{
+                    height: '50vh',
+                    width: 'auto',
+                    margin: '20px',
+                    borderRadius: '20px'
+                }}/>
+                <div className='left' style={{
+                    maxWidth: '35vw'
+                }}>
+                    <p className='small-text' style={{fontWeight: 200}}>THE IDEA</p>
+                    <h2 className='large-text' styles={{fontWeight: 700}}>Create a Visual Schedule</h2>
+                    <p className='small-text' styles={{fontWeight: 500}}>I wanted to create a website where a user could read the schedules at a glance 👀</p>
+                    <p className='small-text' styles={{fontWeight: 500}}></p>
+                </div>
+                <img src={MobileAppConcept} style={{
+                    height: '60vh',
+                    width: 'auto',
+                    marginTop: '10vh',
+                    borderRadius: '20px'
+                }}/>
             </div>
             <div className='hbox mega-margin'>
-                <div className='invisible-card left'>
-                    <h2 className='large-text medium-margin'>The Idea</h2>
-                    <p>I wanted to create a website that was <b>mobile friendly</b> and <b>easy to read</b>. The idea I ultimately landed on was to create a visual schedule, similar to google calendar with the courts at each gym. The different sports would be color coded so that they would be easily recognized.</p>
-                </div>
-                <img src={WebsiteConcept} className='desktop-screenshot'/>
-                <img src={MobileAppConcept} className='desktop-screenshot'/>
-            </div>
-            <div className='hbox mega-margin'>
-                <div className='invisible-card left'>
-                    <h2 className='large-text small-margin'>How I Built</h2>
-                    <a href='www.uwopenrecroster.com'><h2 className='small-text large-margin'>UW Open Rec Roster</h2></a>
-                    <p className='large-margin'>The website is a vite react app hosted on DigitalOceans.com with a domain name from DreamHost.com. The website interacts with a backend to get the dialy schedules from the recwell API. These schedules are memoized serverside daily, as to not overwhelm the recwell API. Additionally, the backend logs requests to track our backend calls. The DB was managed by my friend Bailey Kau.</p>
+                <div className='left' style={{
+                    width: '30vw'
+                }}>
+                    <p className='small-text' style={{fontWeight: 200}}>How I Built</p>
+                    <a href='www.uwopenrecroster.com'><h2 className='large-text' style={{fontWeight: 700}}>UW Open Rec Roster</h2></a>
+                    <p className='small-text' styles={{fontWeight: 500}}>I created the website as a React app. I used DigitalOceans to host the front and backend. I got my domain name from DreamHost.</p>
+                    {/* <p className='large-margin'>The website is a vite react app hosted on DigitalOceans.com with a domain name from DreamHost.com. The website interacts with a backend to get the dialy schedules from the recwell API. These schedules are memoized serverside daily, as to not overwhelm the recwell API. Additionally, the backend logs requests to track our backend calls. The DB was managed by my friend Bailey Kau.</p>
                     <h2 className='medium-text small-margin'>What did I learn while making the website?</h2>
                     <ul>
                         <li><b>CSS Psuedo-Elements</b>: I was having serious issues with my styling and lag. Resizing the window would destroy the positioning of the hour mark lines and times themselves. I did some searching and found psuedo-elements `::before` and `::after`.</li>
                         <li><b>CSS Animations</b>: I wanted to add a skeleton loading animation to my website to show that the data was loading. I was able to learn about animation past the transition attribute.</li>
                         <li><b>Deploying a Web-App and Backend</b>: This project was really the first time I needed to deploy a project. I learned how to deploy not just on the macro-scale with pre-built deployers, but using NGINX. For the backend I had a blank vm, and had to configure it. I also learned how to use domain names in this project, giving both my frontend and backend unique domain names.</li>
-                    </ul>
+                    </ul> */}
                 </div>
-                <img src={WebScreenShot} className='desktop-screenshot'/>
+                <div className='vbox' style={{
+                    marginRight: '1vw'
+                }}>
+                    <img className='digital-oceans' src='https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fpluspng.com%2Fimg-png%2Fdigitalocean-logo-png-open-2000.png&f=1&nofb=1&ipt=5691adca57348f15ddb304f3368b0042ab34898820b74e16f3c5d1d5b0447a34&ipo=images' height='100' width='100'/>
+                    <img className='react' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fseeklogo.net%2Fwp-content%2Fuploads%2F2020%2F09%2Freact-logo.png&f=1&nofb=1&ipt=a316d2c9e3f1784d1cd73ef71b111f8f0b54ab8d089b9ea0981e0b0d53a35bd7&ipo=images' height='100' width='100'/>
+                    <img className='dream-host' src='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.freebiesupply.com%2Flogos%2Flarge%2F2x%2Fdreamhost-logo-png-transparent.png&f=1&nofb=1&ipt=22a54570e8723013b89a9a64110c738ebf9e79c8bf09f7c72e4a1bb694d27c9f&ipo=images' height='80' width='80'/>
+                </div>
+                <img src={WebScreenShot} style={{
+                    width: '40vw',
+                    height: 'auto',
+                    borderRadius: '20px'
+                }}/>
+            </div>
+            <div className='hbox mega-margin'>
+                <img src={MobileScreenShot} style={{
+                    height: '70vh',
+                    width: 'auto',
+                    borderRadius: '20px'
+                }}/>
             </div>
         </div>
     </>
