73d1226760adf6f3b444767f8aa5f5f0abccee8c
Hayden Dippel <hdippel@wisc.edu>
2024-02-25 13:14:25 -0600

    improved BadgerBasketball page

--- src/components/GitHubLink.jsx
+++ src/components/GitHubLink.jsx
@@ -1,14 +1,18 @@
-import '../styles/GitHubLink.css'
-import '../styles.css'
-
 function GitHubLink(props) {
-    
-    return <div className='hbox'>
-        <img className='github-logo' src='https://pngimg.com/uploads/github/github_PNG83.png'/>
-        <a href={props.link} target='_blank'>
-            <p className='small-text'>{props.link}</p>
-        </a>
-    </div>
+
+    return <a href={props.link} style={{margin: '10px'}}>
+        <div className='github-box' style={{
+            height: props.size,
+            width: props.size,
+            borderRadius: props.size * 0.2,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#46c43d'
+        }}>
+            <img height={props.size * 0.95} width={props.size * 0.95} src='https://pngimg.com/uploads/github/github_PNG83.png'/>
+        </div>
+    </a>
 }
 
 export default GitHubLink
--- src/components/projects/BadgerBasketball.jsx
+++ src/components/projects/BadgerBasketball.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect, useRef } from 'react'
+import * as d3 from 'd3'
+
 import BakkeScheduleLedger from '../../assets/BakkeScheduleLedger.png'
 import WebsiteConcept from '../../assets/WebsiteConcept.jpg'
 import MobileAppConcept from '../../assets/MobileAppConcept.jpg'
@@ -5,9 +8,11 @@ import WebScreenShot from '../../assets/WebScreenshot.png'
 import MobileWebScreenShot from '../../assets/MobileWebScreenshot.png'
 
 import GitHubLink from '../GitHubLink'
+import Link from '../Link'
 
 import '../../styles.css'
 import '../../styles/Projects.css'
+import '../../styles/D3.css'
 
 const BADGER_BASKETBALL_GIT_REPO_URL = 'https://github.com/HaydenDippL/BadgerBasketball'
 const UW_OPEN_REC_ROSTER_URL = 'https://www.uwopenrecroster.com'
@@ -18,11 +23,36 @@ const ANDROID_LOGO_URL = 'https://external-content.duckduckgo.com/iu/?u=https%3A
 const APPLE_LOGO_URL = 'https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Flofrev.net%2Fwp-content%2Fphotos%2F2016%2F05%2Frainbow-apple-logo.png&f=1&nofb=1&ipt=c42589023ade19f27d32af95a76eb8d965e225c53f38bd0be3b03954aa50695d&ipo=images'
 
 function BadgerBasketball() {
+    const [analytics, set_analytics] = useState(null)
+
+    useEffect(() => {
+        const params = new URLSearchParams()
+        params.append('get_total_visits', true)
+        params.append('get_total_users', true)
+        params.append('get_total_queries', true)
+        params.append('get_daily_users', true)
+
+        fetch("https://www.uwopenrecrosterbackend.xyz/analytics?" + params)
+            .then(res => res.json())
+            .then(data => {
+                data.daily_users.sort((a, b) => new Date(a.date_of_queries) - new Date(b.date_of_queries))
+                data.daily_users.forEach(d => d.date_of_queries = d.date_of_queries.substring(0, 10))
+                set_analytics(data)
+            })
+    }, [])
+
+    useEffect(() => {
+        const line_plot_div = document.getElementById('daily-visits-line-plot')
+        if (line_plot_div && line_plot_div.children.length <= 0 && analytics !== null) daily_visits_line_plot(analytics.daily_users)
+    }, [analytics])
 
     return <div className='dark centered'>
         <h1 className='title'>UW Open Rec Roster</h1>
-        <GitHubLink link={BADGER_BASKETBALL_GIT_REPO_URL}/>
-        <div className='hbox' style={{marginTop: '30vh'}}>
+        <div className='hbox'>
+            <GitHubLink link={BADGER_BASKETBALL_GIT_REPO_URL} size={70}/>
+            <Link link={UW_OPEN_REC_ROSTER_URL} size={70}/>
+        </div>
+        <div className='hbox' style={{marginTop: '10vh'}}>
             <div className='left' style={{width: '40vw'}}>
                 <h2 className='pre-header'>THE PROBLEM</h2>
                 <p className='header'>The gym websites were extremely user-unfriendly</p>
@@ -42,8 +72,8 @@ function BadgerBasketball() {
             }}/>
         </div>
         <div className='hbox' style={{
-            marginTop: '25vh',
-            marginBottom: '25vh'
+            marginTop: '10vh',
+            marginBottom: '10vh'
         }}>
             <img src={WebsiteConcept} style={{
                 height: '50vh',
@@ -66,10 +96,12 @@ function BadgerBasketball() {
             }}/>
         </div>
         <div className='hbox mega-margin'>
-            <div className='left' style={{width: '30vw'}}>
+            <div className='left' style={{width: '30vw', marginBottom: '-90px'}}>
                 <p className='pre-header'>How I Built</p>
-                <a href={UW_OPEN_REC_ROSTER_URL}><h2 className='header'>UW Open Rec Roster</h2></a>
-                <p className='text'>I created the website as a React app. I used DigitalOceans to host the front and backend. I got my domain name from DreamHost.</p>
+                <a href={UW_OPEN_REC_ROSTER_URL} style={{marginBottom: '5vh'}}><h2 className='header'>UW Open Rec Roster</h2></a>
+                {/* <p className='text'>I created the website as a React app. I used DigitalOceans to host the front and backend. I got my domain name from DreamHost.</p> */}
+                <p className='text'>I created the website as a React app, hosted on DigitalOceans with a domain name from DreamHost. It is able to display all 8 court schedules for each of the gyms at UW-Madison: Bakke and Nick.</p>
+                <p className='text'>The backend uses server-size memoization to more efficiently handle requests, and uses logging to track user activity on the site.</p>
             </div>
             <div className='vbox' style={{marginRight: '1vw'}}>
                 <img className='digital-oceans' src={DIGITAL_OCEANS_LOGO_URL} height='100' width='100'/>
@@ -103,7 +135,211 @@ function BadgerBasketball() {
                 </div>
             </div>
         </div>
+        <div className='vbox mega-margin'>
+            <h2 className='big-header'>And People Actually Use It</h2>
+            <div className='hbox'>
+                <div id='daily-visits-line-plot' style={{height: '370px'}}/>
+                <div className='hbox header'>
+                    <div className='left'>
+                        <p className='close-text fat'>Total Users</p>
+                        <p className='close-text fat'>Total Visits</p>
+                        <p className='close-text fat'>Schedules Displayed</p>
+                    </div>
+                    <div className='left'>
+                        {analytics && <p className='close-text skinny'>{analytics.total_users}</p>}
+                        {analytics && <p className='close-text skinny'>{analytics.total_visits}</p>}
+                        {analytics && <p className='close-text skinny'>{analytics.total_queries}</p>}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+}
+
+function daily_visits_line_plot(data) {
+
+    // Set dimensions and margins for the chart
+    const margin = { top: 70, right: 30, bottom: 40, left: 80 };
+    const width = 770 - margin.left - margin.right;
+    const height = 400 - margin.top - margin.bottom;
+
+    // Set up the x and y scales
+    const x = d3.scaleTime()
+        .range([0, width]);
+
+    const y = d3.scaleLinear()
+        .range([height, 0]);
+
+    // Set up the line generator
+    const line = d3.line()
+        .x(d => x(d.date_of_queries))
+        .y(d => y(d.daily_users));
+
+    // Create the SVG element and append it to the chart container
+    const svg = d3.select('#daily-visits-line-plot')
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // create tooltip div
+
+    const tooltip = d3.select('#daily-visits-line-plot')
+        .append("div")
+        .attr("class", "tooltip");
+
+    // Parse the date and convert the population to a number
+    const parseDate = d3.timeParse("%Y-%m-%d");
+    data.forEach(d => {
+        d.date_of_queries = parseDate(d.date_of_queries);
+    });
+
+    // Set the domains for the x and y scales
+    x.domain(d3.extent(data, d => d.date_of_queries));
+    y.domain([0, d3.max(data, d => d.daily_users)]);
+
+    // Add the x-axis
+    svg.append("g")
+        .attr("transform", `translate(0,${height})`)
+        .style("font-size", "14px")
+        .call(d3.axisBottom(x)
+            .tickValues(x.ticks(d3.timeDay.every(1))) // Display ticks every 6 months
+            .tickFormat(d3.timeFormat("%b %d"))) // Format the tick labels to show Month and Year
+        .call(g => g.select(".domain").remove()) // Remove the x-axis line
+        .selectAll(".tick line") // Select all tick lines
+        .style("stroke-opacity", 0)
+    svg.selectAll(".tick text")
+        .attr("fill", "#777");
+
+    // Add vertical gridlines
+    svg.selectAll("xGrid")
+        .data(x.ticks().slice(1, -1))
+        .join("line")
+        .attr("x1", d => x(d))
+        .attr("x2", d => x(d))
+        .attr("y1", 0)
+        .attr("y2", height)
+        .attr("stroke", "#e0e0e0")
+        .attr("stroke-width", .5);
+
+    // Add the y-axis
+    svg.append("g")
+        .style("font-size", "14px")
+        .call(d3.axisLeft(y)
+        .ticks((d3.max(data, d => d.daily_users)) / 10)
+        // .tickFormat(d => {
+        //     if (isNaN(d)) return "";
+        //     return `${(d / 1000).toFixed(0)}k`;
+        // })
+        .tickSize(0)
+        .tickPadding(10))
+        .call(g => g.select(".domain").remove()) // Remove the y-axis line
+        .selectAll(".tick text")
+        .style("fill", "#777") // Make the font color grayer
+        .style("visibility", (d, i, nodes) => {
+            if (i === 0) {
+                return "hidden"; // Hide the first and last tick labels
+            } else {
+                return "visible"; // Show the remaining tick labels
+            }
+        });
+
+    // Add Y-axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .style("font-size", "14px")
+        .style("fill", "#777")
+        .style("font-family", "sans-serif")
+        .text("Visits");
+
+    // Add horizontal gridlines
+    svg.selectAll("yGrid")
+        .data(y.ticks((d3.max(data, d => d.daily_users)) / 10).slice(1))
+        .join("line")
+        .attr("x1", 0)
+        .attr("x2", width)
+        .attr("y1", d => y(d))
+        .attr("y2", d => y(d))
+        .attr("stroke", "#e0e0e0")
+        .attr("stroke-width", .5)
+
+    // Add the line path
+    const path = svg.append("path")
+        .datum(data)
+        .attr("d", line);
+
+    // Add a circle element
+
+    const circle = svg.append("circle")
+        .attr("r", 0)
+        .attr("fill", "steelblue")
+        .style("stroke", "white")
+        .attr("opacity", .70)
+        .style("pointer-events", "none");
+    // create a listening rectangle
+
+    const listeningRect = svg.append("rect")
+        .attr("width", width)
+        .attr("height", height)
+        .classed("listening-rect", true)
+
+    // create the mouse move function
+
+    listeningRect.on("mousemove", function (event) {
+        const [xCoord] = d3.pointer(event, this);
+        const bisectDate = d3.bisector(d => d.date_of_queries).left;
+        const x0 = x.invert(xCoord);
+        const i = bisectDate(data, x0, 1);
+        const d0 = data[i - 1];
+        const d1 = data[i];
+        const d = x0 - d0.date_of_queries > d1.date_of_queries - x0 ? d1 : d0;
+        const xPos = x(d.date_of_queries);
+        const yPos = y(d.daily_users);
+
+
+        // Update the circle position
+
+        circle.attr("cx", xPos)
+        .attr("cy", yPos);
+
+        // Add transition for the circle radius
+
+        circle.transition()
+        .duration(50)
+        .attr("r", 5);
+
+        // add in  our tooltip
+
+        tooltip
+        .style("display", "block")
+        .style("left", `${xPos}px`)
+        .style("top", `${yPos}px`)
+        .html(`<strong>Date:</strong> ${d.date_of_queries.toLocaleDateString()}<br><strong>Daily Visits:</strong> ${d.daily_users}`)
+    });
+    // // listening rectangle mouse leave function
+
+    listeningRect.on("mouseleave", function () {
+        circle.transition()
+        .duration(50)
+        .attr("r", 0);
+
+        tooltip.style("display", "none");
+    });
+
+    // Add the chart title
+    svg.append("text")
+        .attr("class", "chart-title")
+        .attr("x", margin.left + 160)
+        .attr("y", margin.top - 100)
+        .style("font-size", "24px")
+        .style("font-weight", "bold")
+        .style("font-family", "sans-serif")
+        .text("Daily Visits");
 }
 
 export default BadgerBasketball
