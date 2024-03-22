79b49d94e630e6eafa1ff741a9474bbecea767c2
Hayden Dippel <hdippel@wisc.edu>
2024-03-22 16:36:21 -0500

    added linkedin

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -5,6 +5,7 @@ function ContactMe() {
         <h1 className='title'>Contact Me</h1>
         <p className='large-text'>hdippel@wisc.edu</p>
         <p className='large-text'>(262)-455-8711</p>
+        <p className='large-text'><a  target='_blank' href='https://www.linkedin.com/in/hayden-dippel/'>https://www.linkedin.com/in/hayden-dippel/</a></p>
     </div>
 }
 
