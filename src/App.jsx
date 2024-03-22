fd7822a82b2faa781dcdc250a0d9f9a7b4c0fc2c
Hayden Dippel <hdippel@wisc.edu>
2024-03-22 12:35:06 -0500

    prompt users go on laptop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import HelloWorld from './components/HelloWorld'
 import AboutMe from './components/AboutMe'
 import Projects from './components/Projects'
@@ -6,11 +8,31 @@ import ContactMe from './components/ContactMe'
 import './App.css'
 
 function App() {
+    const [mobile, set_mobile] = useState(false)
+
+    useEffect(() => {
+        function handle_resize() {
+            set_mobile(window.innerWidth < 1000)
+            console.log(window.innerWidth)
+        }
+
+        handle_resize()
+        window.addEventListener('resize', handle_resize)
+        return () => window.addEventListener('resize', handle_resize)
+    }, [])
+
     return <>
-        <HelloWorld/>
-        <AboutMe/>
-        <Projects/>
-        <ContactMe/>
+        { !mobile ? 
+        <>
+            <HelloWorld/>
+            <AboutMe/>
+            <Projects/>
+            <ContactMe/>
+        </> :
+        <p style={{fontSize: '60px'}}>
+            This site is best experienced on a wider screen. Try expanding your window if on a computer, or switching to your computer if you are on mobile
+        </p>
+        }
     </>
 }
 
